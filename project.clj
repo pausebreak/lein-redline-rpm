0a1890d1d20cb80d07573468e099323dfebb7d46
Chris Koskey <ckoskey@tripwire.com>
2016-01-12 10:17:25 -0800

    update to redline 1.2.2 and bump our version to 0.4.3

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-redline-rpm "0.4.2"
+(defproject lein-redline-rpm "0.4.3"
   :description "Create an RPM without rpm-tools"
   :url "https://github.com/pausebreak/lein-redline-rpm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.redline-rpm/redline "1.2.1"]]
+  :dependencies [[org.redline-rpm/redline "1.2.2"]]
   :eval-in-leiningen true)
