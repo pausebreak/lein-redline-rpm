12afa83e0f1ac23853d170ea5b4b961e6b87b05a
Marcus Crestani <marcus@crestani.de>
2018-09-26 11:35:31 +0200

    Add dependency to `commons-compress` that redline uses.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/pausebreak/lein-redline-rpm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.redline-rpm/redline "1.2.2"]]
+  :dependencies [[org.redline-rpm/redline "1.2.4"]
+                 [org.apache.commons/commons-compress "1.18"]]
   :eval-in-leiningen true)
